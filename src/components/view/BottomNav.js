43e50182a2c0659143027995a5650ed315a64ec2
Evans Owino <evansowino@hotmail.com>
2018-09-29 12:02:20 +0100

    bottom nav styles

--- src/components/view/BottomNav.js
+++ src/components/view/BottomNav.js
@@ -2,18 +2,22 @@ import React from 'react';
 import { Button, Icon } from '..';
 
 export class BottomNav extends React.PureComponent {
+  get buttonStyles() {
+    return 'bottom-nav__btn btn--remove-default'
+  }
+  
   render() {
     return (
       <footer className="bar bottom-nav">
-        <Button onClick={this.props.onClick}>
+        <Button onClick={this.props.onClick} className={this.buttonStyles}>
           <Icon>shopping_cart</Icon>
           <span>Cart</span>
         </Button>
-        <Button onClick={this.props.onClick}>
+        <Button onClick={this.props.onClick} className={this.buttonStyles}>
           <Icon>camera_alt</Icon>
           <span>Scan</span>
         </Button>
-        <Button onClick={this.props.onClick}>
+        <Button onClick={this.props.onClick} className={this.buttonStyles}>
           <Icon>person</Icon>
           <span>Profile</span>
         </Button>
