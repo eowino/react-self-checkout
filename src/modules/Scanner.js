372deff03734ac886476b443a293807b6cbd4bfe
Evans Owino <evansowino@hotmail.com>
2018-10-16 12:38:50 +0100

    removing debugging markup from Scanner view

--- src/modules/Scanner.js
+++ src/modules/Scanner.js
@@ -24,7 +24,6 @@ export class Scanner extends React.Component {
   }
 
   render() {
-    const { title, barcode } = this.state;
     return (
       <React.Fragment>
         <Link to="/" className="back-link align-center"><Icon>arrow_back</Icon>Back</Link>
@@ -32,18 +31,6 @@ export class Scanner extends React.Component {
           <div className="overlay__video">
             <div className="overlay__center-line" />
             <video id="video" height="100%" width="100%" />
-          </div>
-          <div className="position-page-bottom overlay__text">
-            <p>Scan a barcode</p>
-            <p>
-              <strong>Barcode is: {barcode}</strong>
-            </p>
-            {title &&
-              title.length > 0 && (
-                <p>
-                  <strong>Barcode is: {title}</strong>
-                </p>
-              )}
           </div>
         </Body>
       </React.Fragment>
